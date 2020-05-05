81d342f5ddfb1add931f84b3458230f0abc572fa
Josh Ford <thisisjoshford@gmail.com>
2020-05-05 09:21:10 -0700

    search test

---
+++ src/components/Search/Search.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Search from './Search';
+
+describe('Search component', () => {
+  it('renders Search', () => {
+    const wrapper = shallow(<Search />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});

7e22a44765a414b9309b949780193bb622aa8d87
Josh Ford <thisisjoshford@gmail.com>
2020-05-05 09:27:06 -0700

    extension fix

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Search from '../components/Search/Search';
+import Search from '../components/Search/Search.jsx';
 import { useHistory } from 'react-router-dom';
 
 export default function Main() {

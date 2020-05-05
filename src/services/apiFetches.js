4be11f732badc3b4df8f07d2f4c5f5e285a687c1
Josh Ford <thisisjoshford@gmail.com>
2020-05-04 17:44:13 -0700

    munge

--- src/services/apiFetches.js
+++ src/services/apiFetches.js
@@ -1,7 +1,8 @@
 export const getArtists = (artist) => {
   return fetch(`http://musicbrainz.org/ws/2/artist?query=${artist}&fmt=json&limit=25'`)
-    .then(res => res.json().map(artist => {
+    .then(res => res.json())
+    .then(json => json.artists.map(artist => ({
       id: artist.id,
-      artistName: artist.name
-    }));
+      name: artist.name
+    })));
 };

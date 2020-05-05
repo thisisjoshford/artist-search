2b8539b605e67a2d62c44d2e63e5b91fd690338c
Josh Ford <thisisjoshford@gmail.com>
2020-05-05 10:16:19 -0700

    moved link

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import withList from '../withList';
 
-const Artist = ({ name, info }) => (
+const Artist = ({ name, info, id }) => (
   <>
-    <p>{name}</p>
+    <Link to={`/${name}/${id}`} >
+      <p>{name}</p>
+    </Link>
     <p>Info: {info}</p>
   </>
 );
@@ -14,6 +17,7 @@ const SearchResults = withList(Artist, 'results');
 Artist.propTypes = {
   name: PropTypes.string.isRequired,
   info: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired
 };
 
 export default SearchResults;
--- src/components/withList.js
+++ src/components/withList.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const withList = (ListItemComponent, propName = 'list') => {
 
   function CreateListComponent(props) {
     const listItems = props[propName].map(item => (
       <li key={item.id}>
-        <Link to={`/${item.name}/${item.id}`} >
-          <ListItemComponent {...item} />
-        </Link>
+        <ListItemComponent {...item} />
       </li>
     ));
 
